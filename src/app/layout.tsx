67c361df8076cf016781d719383a11badc3625de
unknown <aanwidianto01@gmail.com>
2025-01-16 21:14:43 +0700

    add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,49 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 import "./globals.css";
-import ClientLayout from '@/components/layouts/client-layout';
+import ClientLayout from "@/components/layouts/client-layout";
 import React from "react";
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://sukses-mandiri-alka.vercel.app"),
-    title: 'Sukses Mandiri Alka',
-    description: 'Website UMKM Sukses Mandiri untuk Aluminium dan Kaca',
-    keywords: ['aluminium', 'kaca', 'umkm'],
-    openGraph: {
-        title: 'Sukses Mandiri Alka',
-        description: 'Website UMKM Sukses Mandiri untuk Aluminium dan Kaca',
-        url: 'https://sukses-mandiri-alka.vercel.app',
-        siteName: 'Sukses Mandiri Alka',
-        images: ['/image/logo/logo.svg'],
-    },
-    icons: {
-        icon: [
-            { url: '/image/logo/logo.svg', type: 'image/svg+xml' },
-            { url: '/image/logo/logo.png', type: 'image/png' }
-        ]
-    }
-}
+  metadataBase: new URL("https://sukses-mandiri-alka.vercel.app"),
+  title: "Sukses Mandiri Alka",
+  description: "Website UMKM Sukses Mandiri untuk Aluminium dan Kaca",
+  keywords: ["aluminium", "kaca", "umkm"],
+  verification: {
+    google: "PZluAmRpWGTONUDm8SXAbz90rFzL3Z2IkidGKUOFr14",
+  },
+  openGraph: {
+    title: "Sukses Mandiri Alka",
+    description: "Website UMKM Sukses Mandiri untuk Aluminium dan Kaca",
+    url: "https://sukses-mandiri-alka.vercel.app",
+    siteName: "Sukses Mandiri Alka",
+    images: ["/image/logo/logo.svg"],
+  },
+  icons: {
+    icon: [
+      { url: "/image/logo/logo.svg", type: "image/svg+xml" },
+      { url: "/image/logo/logo.png", type: "image/png" },
+    ],
+  },
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className="scroll-smooth">
-        <body
-            style={{
-                width: "100%",
-                height: "100vh",
-                backgroundImage: "url('/image/background/bg.svg')",
-                backgroundRepeat: "repeat",
-                backgroundSize: "auto",
-            }}
-        >
-        <ClientLayout>
-            {children}
-        </ClientLayout>
-        </body>
-        </html>
-    );
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundImage: "url('/image/background/bg.svg')",
+          backgroundRepeat: "repeat",
+          backgroundSize: "auto",
+        }}
+      >
+        <ClientLayout>{children}</ClientLayout>
+      </body>
+    </html>
+  );
 }
